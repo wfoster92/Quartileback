88a78608c37e596907c944192b2c764e2d38b2dd
Will Foster <wfoster92@gmail.com>
2023-11-11 22:45:05 -0500

    added craps to homepage

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,22 +1,42 @@
-// Components/NavBar.js
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { AppBar, Toolbar, Typography, IconButton } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
 
-const NavBar = () => {
+export default function NavBar() {
+  // const classes = useStyles()
+  const [example, setExample] = useState('success')
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contacts</Link>
-        </li>
-        <li>
-          <Link to="/products">Products</Link>
-        </li>
-      </ul>
-    </nav>
+    <React.Fragment>
+      <AppBar
+        color={example}
+        // color={isCustomColor || isCustomHeight ? 'primary' : example}
+        // className={`${isCustomColor && classes.customColor} ${
+        //   isCustomHeight && classes.customHeight
+        // }`}
+      >
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <IconButton color="inherit" onClick={() => setExample('success')}>
+            Gamblin'
+          </IconButton>
+          <IconButton color="inherit" onClick={() => setExample('primary')}>
+            Winnin'
+          </IconButton>
+          <IconButton color="inherit" onClick={() => setExample('error')}>
+            Losin'
+          </IconButton>
+          <IconButton color="inherit" onClick={() => setExample('warning')}>
+            Bettin'
+          </IconButton>
+          <IconButton color="inherit" onClick={() => setExample('transparent')}>
+            Sports!
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+    </React.Fragment>
   )
 }
-
-export default NavBar
