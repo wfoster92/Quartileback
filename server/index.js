4e8b7920c18a7cb605ff4638696f409aa951a241
Will Foster <wfoster92@gmail.com>
2023-11-12 11:39:16 -0500

    updates on the server side

--- server/index.js
+++ server/index.js
@@ -1,13 +1,29 @@
 // server/index.js
 
 const express = require('express')
+const fs = require('fs')
+const csv = require('csvtojson')
 
 const PORT = process.env.PORT || 3001
 
 const app = express()
 
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from server!' })
+let conferenceRatingsFile = './csvs/conference_ratings_2023-11-04.csv'
+
+app.get('/api', async (req, res) => {
+  try {
+    let result = await csv()
+      .fromFile(conferenceRatingsFile)
+      .then((jsonObj) => {
+        console.log(jsonObj)
+        return jsonObj // return the JSON data
+      })
+
+    res.json(result) // send the JSON data as the response
+  } catch (error) {
+    console.error('Error reading CSV file:', error)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
 })
 
 app.listen(PORT, () => {
