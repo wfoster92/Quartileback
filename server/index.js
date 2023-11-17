330f67411fb97ada9ad81675ebc0043302a79f41
Will Foster <wfoster92@gmail.com>
2023-11-17 18:32:07 -0500

    updated favicon, wrote backend code for spread, overUnder

--- server/index.js
+++ server/index.js
@@ -8,15 +8,41 @@ const PORT = process.env.PORT || 3001
 
 const app = express()
 
-let conferenceRatingsFile = './csvs/conference_ratings_2023-11-04.csv'
+let conferenceRatingsFile = './csvs/ND_vs_WAKE.csv'
 
 app.get('/api', async (req, res) => {
   try {
     let result = await csv()
       .fromFile(conferenceRatingsFile)
-      .then((jsonObj) => {
-        console.log(jsonObj)
-        return jsonObj // return the JSON data
+      .then((data) => {
+        // get the home and away team from the filename
+        const [home, away] = conferenceRatingsFile
+          .split('/')
+          .pop()
+          .split('.')[0]
+          .split('_vs_')
+        console.log(home, away)
+        let overUnder = {}
+        let spread = {}
+        data.forEach((row) => {
+          // console.log(JSON.stringify(row))
+          let diff = Number(row[home]) - Number(row[away])
+          let sum = Number(row[home]) + Number(row[away])
+          // console.log(diff, sum)
+          if (spread[diff]) {
+            spread[diff] += Number(row.probability)
+          } else {
+            spread[diff] = Number(row.probability)
+          }
+          if (overUnder[sum]) {
+            overUnder[sum] += Number(row.probability)
+          } else {
+            overUnder[sum] = Number(row.probability)
+          }
+        })
+        console.log(JSON.stringify(overUnder, null, 2))
+        console.log(JSON.stringify(spread, null, 2))
+        return { spread: spread, overUnder: overUnder } // return the JSON data
       })
 
     res.json(result) // send the JSON data as the response
