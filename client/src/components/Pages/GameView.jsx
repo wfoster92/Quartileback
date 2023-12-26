0e76b2fe8fc56bfe11a93ae2567bcb2313fbcc3e
Will Foster <wfoster92@gmail.com>
2023-12-26 17:19:31 -0500

    switched the axis labels in the heatmap, centered the heatmap

--- client/src/components/Pages/GameView.jsx
+++ client/src/components/Pages/GameView.jsx
@@ -394,6 +394,7 @@ const GameView = () => {
                 style={{
                   boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
                   margin: '2vw',
+                  textAlign: 'center',
                 }}
               >
                 <HeatmapContainer
